20f37f9b1ced5cbd7ab9105ca70e97cb9220fc90
Sabir Ali Shah <sabir.shah@hotmail.com>
2025-06-08 08:34:34 +1200

    Initial commit with proper .gitignore

---
+++ ParcelScanProcessing.ScanWorker/Program.cs
@@ -0,0 +1,7 @@
+using ParcelScanProcessing.ScanWorker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
